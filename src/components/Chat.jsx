79061569bbf9de0f52a9154d2e35e0cff23844f8
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2023-09-29 20:55:17 +0000

    added dynamic chat text from firestore

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,4 +1,6 @@
-import React, { useRef } from "react";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import React, { useEffect, useRef, useState } from "react";
+import { db } from "../firebase";
 import Message from "./Message";
 
 const style = {
@@ -6,13 +8,34 @@ const style = {
 };
 
 const Chat = () => {
+	const [messages, setMessages] = useState([]);
 	const scroll = useRef();
+
+	useEffect(() => {
+		const q = query(collection(db, "messages"), orderBy("timestamp"));
+		const unsubscribe = onSnapshot(q, (querySnapshot) => {
+			let messages = [];
+			querySnapshot.forEach((doc) => {
+				messages.push({ ...doc.data(), id: doc.id });
+			});
+			setMessages(messages);
+		});
+		return () => {
+			unsubscribe();
+		};
+	}, []);
+
 	return (
 		<>
 			<main className={style.main}>
-                {/* chat Message */}
-                <Message />
-            </main>
+				{/* chat Message */}
+                {messages.map((message) => (
+                    <Message
+                        key={message.id}
+                        message={message}
+                    />
+                ))}
+			</main>
 			{/* Send Message */}
 			<span ref={scroll}></span>
 		</>
